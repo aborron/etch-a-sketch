973389fb9228b76b654e9199fdaaeac60fe0fdb3
Lexie <152320499+lborron@users.noreply.github.com>
2023-12-20 23:36:32 -0600

    Add grid size button styles and functionality

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 const container = document.querySelector('.container')
+const gridSizeButton = document.querySelector('#grid-size')
 
-for (let y = 0; y < 16; y++) {
-    const row = document.createElement('div')
-    row.classList.add('row')
-    container.appendChild(row)
+// Default grid size
+let gridSize = 16;
 
-    for (let x = 0; x < 16; x++) {
-        const div = document.createElement('div')
-        row.appendChild(div)
+createGrid(gridSize)
+
+function getGridSize() {
+    gridSize = prompt('Enter number of squares:')
+
+    if (gridSize === null) {
+        return
+    }
+
+    gridSize = parseInt(gridSize);
+
+    if (isNaN(gridSize) || gridSize <= 0 || gridSize > 100) {
+        alert('Please enter a valid number between 1 and 100.');
+        return;
+    }
+
+    clearContainer()
+    createGrid(gridSize)
+}
+
+function clearContainer() {
+    container.innerHTML = ''
+}
+
+function createGrid(gridSize) {
+    for (let y = 0; y < gridSize; y++) {
+        const row = document.createElement('div')
+        row.classList.add('row')
+        container.appendChild(row)
+
+        for (let x = 0; x < gridSize; x++) {
+            const div = document.createElement('div')
+            row.appendChild(div)
+        }
     }
 }
 
+gridSizeButton.addEventListener('click', getGridSize)
